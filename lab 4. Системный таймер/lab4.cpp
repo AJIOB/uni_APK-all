abc3336d07023ece170c1d26753ad7d25de97f10
AJIOB <aleksandr9809@gmail.com>
2017-04-10 17:45:49 +0300

    lab4: state words are printing now at begin and end of program

--- lab 4. Системный таймер/lab4.cpp
+++ lab 4. Системный таймер/lab4.cpp
@@ -1,4 +1,12 @@
+/*
++ 1) c помощью системного таймера генерирует звук заданной частоты (по вариантам);
++ 2) выводит слово состояния для каждого канала в двоичном виде;
+  3) определяет коэффициент деления для каждого канала в 16-ричном виде.
+  *Реализовать генератор случайных чисел от нуля до заданного с клавиатуры числа.
+*/
+
 #include <dos.h>
+#include <stdio.h>
 
 const long int maxValue = 1193180;
 
@@ -51,6 +59,17 @@ const int task[] = {
 	329, 400
 };
 
+struct oneByte{
+	unsigned int b0 : 1;
+	unsigned int b1 : 1;
+	unsigned int b2 : 1;
+	unsigned int b3 : 1;
+	unsigned int b4 : 1;
+	unsigned int b5 : 1;
+	unsigned int b6 : 1;
+	unsigned int b7 : 1;
+};
+
 void playOneBeep(int f, int sleepTime)
 {
 	int delim;
@@ -92,8 +111,37 @@ void playSound()
 	}
 }
 
+void printStateWords()
+{
+	int i;
+	int port = 0x40;
+	int valuep;
+	int delta = 2;
+	struct oneByte* ob = (oneByte*)&valuep;
+
+	for (i = 0; i < 3; ++i, delta *= 2, port++)
+	{
+		outp(0x43, 0xE0 + delta);			//11 1(not save CE - current state of counter) 0(read value of channel) number_of_channel 0
+		valuep = inp(port);
+		printf("Port 0x%x: %d%d%d%d%d%d%d%d\n", port, ob->b7, ob->b6, ob->b5, ob->b4, ob->b3, ob->b2, ob->b1, ob->b0);
+	}
+}
+
+void printDelims()
+{
+	//todo
+}
+
 int main()
 {
+	printf("Start values:\n");
+	printStateWords();
+	printDelims();
+
 	playSound();
+
+	printf("\nEnd values:\n");
+	printStateWords();
+	printDelims();
 	return 0;
 }
